83abc9697f7269d4088a86c84791b004bb30dea0
wooyoung <rnjsdndud00@naver.com>
2023-02-15 19:24:01 +0900

    517p_Second

--- src/Stream/Array2StreamDemo.java
+++ src/Stream/Array2StreamDemo.java
@@ -1,24 +1,25 @@
-// StreamFilter_516p
+// Stream_517p_Second
 package Stream;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
 
 public class Array2StreamDemo {
     public static void main(String[] args) {
-        List<String> name = List.of("홍길동", "배정화", "임꺽정", "연흥부", "김선달", "황진이");
-        Stream<String> s1 = name.stream();
-        s1.filter(n -> n.charAt(0) < '이').forEach(n -> System.out.print(n + " "));
-        System.out.println();
-        Stream<String> s2 = name.stream();
-        s2.sorted().forEach(n -> System.out.print(n + " "));
-        System.out.println();
-        Stream<String> s3 = name.stream();
-        System.out.println(s3.findFirst());
-        Stream<String> s4 = name.stream();
-        System.out.println(s4.findFirst().get());
-        Stream<String> s5 = name.stream();
-        System.out.println(s5.count());
+        enum Gender { 남,여 }
+        List<Integer> ages = List.of(25, 20, 29, 28, 32, 18);
+        List<Gender> genders = List.of(Gender.남, Gender.여, Gender.남, Gender.남, Gender.남, Gender.여);
+
+        Stream<Integer> as = ages.stream();
+        System.out.println(as.reduce(Integer::sum).get());
+        Stream<Integer> ax = ages.stream();
+        System.out.println(ax.max(Integer::compare).get());
+        IntStream is = ages.stream().mapToInt(a -> a.intValue());
+        System.out.println(is.average().getAsDouble());
 
     }
 }
